1cfa408f9e550e546b3e1d876925affae240645c
Odisse59 <78123269+Odisse59@users.noreply.github.com>
2021-06-22 19:16:41 -0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,6 +1,6 @@
 var balloon,balloonImage1,balloonImage2;
-// create database and position variable here
-var database, position;
+// create database and Height variable here
+var database, height;
 
 function preload(){
    bg =loadImage("cityImage.png");
@@ -19,8 +19,8 @@ function setup() {
   balloon.addAnimation("hotAirBalloon",balloonImage1);
   
 
-  var balloonPosition=database.ref('balloon/position')
-balloonPosition.on("value",readPosition,showError)
+  var balloonHeight=database.ref('balloon/height')
+balloonHeight.on("value",readHeight,showError)
 balloon.scale=0.4;
 
   textSize(20); 
@@ -31,27 +31,27 @@ function draw() {
   background(bg);
 
   if(keyDown(LEFT_ARROW)){
-    balloon.addAnimation("hotAirBalloon",balloonImage2);
     //write code to move air balloon in left direction
-    changePosition(-3,0);
+    updateHeight(-3,0);
+    balloon.addAnimation("hotAirBalloon",balloonImage2);
   }
   else if(keyDown(RIGHT_ARROW)){
-    balloon.addAnimation("hotAirBalloon",balloonImage2);
     //write code to move air balloon in right direction
-    changePosition(3,0);
+    updateHeight(3,0);
+    balloon.addAnimation("hotAirBalloon",balloonImage2);
   }
   else if(keyDown(UP_ARROW)){
-    balloon.addAnimation("hotAirBalloon",balloonImage2);
     //write code to move air balloon in up direction
-    changePosition(0,-3);
+    updateHeight(0,-3);
+    balloon.addAnimation("hotAirBalloon",balloonImage2);
     balloon.scale=0.5;
 
     
   }
   else if(keyDown(DOWN_ARROW)){
-    balloon.addAnimation("hotAirBalloon",balloonImage2);
     //write code to move air balloon in down direction
-    changePosition(0,+3);
+    updateHeight(0,+3);
+    balloon.addAnimation("hotAirBalloon",balloonImage2);
     balloon.scale=0.3;
 
   }
@@ -63,17 +63,17 @@ function draw() {
   text("**Use arrow keys to move Hot Air Balloon!",40,40);
 }
 
-function changePosition(x,y){
-  database.ref('balloon/position').set({
- 'x':position.x+x,
- 'y':position.y+y
+function updateHeight(x,y){
+  database.ref('balloon/height').set({
+ 'x':height.x+x,
+ 'y':height.y+y
   })
 }
 
-function readPosition(data){
- position=data.val()
- balloon.x=position.x;
- balloon.y=position.y;
+function readHeight(data){
+ height=data.val()
+ balloon.x=height.x;
+ balloon.y=height.y;
 }
 
 function showError(){
